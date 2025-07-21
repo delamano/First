f80c5b33763fa90563ab606b33b4c82cf3f5b9ff
Mano <arthur.de.la.mano@accenture.com>
2025-07-21 13:15:10 -0300

    treinamentojavascript01

---
+++ treinamento01.js
@@ -0,0 +1,11 @@
+let nome = "Arthur";
+const nascimento = 1993;
+let anoAtual = 2025;
+let idade = anoAtual - nascimento;
+let altura = 1.70;
+let estudante = true;
+
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Altura:", altura);
+console.log("É estudante?", estudante);
